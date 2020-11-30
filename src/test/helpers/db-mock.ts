65ac926ca687c389acbec0b5cfc41d1f2e9d255e
Jonny Burger <jonathanburger11@gmail.com>
2020-11-30 09:14:08 +0100

    fix

--- src/test/helpers/db-mock.ts
+++ src/test/helpers/db-mock.ts
@@ -1,69 +1,64 @@
-import randomstring from 'randomstring';
 import {MongoClient} from 'mongodb';
+import randomstring from 'randomstring';
 
 export const create = async (): Promise<[string, string]> => {
 	const dbName = 'test_' + randomstring.generate(5);
 	const userName = 'test_user' + randomstring.generate(5);
-	await new Promise(
-		(resolve, reject): void => {
-			MongoClient.connect(
-				'mongodb://localhost:27017',
-				{
-					useNewUrlParser: true
-				},
-				(err, client): void => {
-					if (err) {
-						return reject(err);
-					}
-					client.db(dbName).addUser(
-						userName,
-						'test',
-						{
-							roles: [{role: 'readWrite', db: dbName}]
-						},
-						(err2: Error): void => {
-							if (err2) {
-								return reject(err2);
-							}
-							resolve();
-						}
-					);
+	await new Promise<void>((resolve, reject): void => {
+		MongoClient.connect(
+			'mongodb://localhost:27017',
+			{
+				useNewUrlParser: true,
+			},
+			(err, client): void => {
+				if (err) {
+					return reject(err);
 				}
-			);
-		}
-	);
+				client.db(dbName).addUser(
+					userName,
+					'test',
+					{
+						roles: [{role: 'readWrite', db: dbName}],
+					},
+					(err2: Error): void => {
+						if (err2) {
+							return reject(err2);
+						}
+						resolve();
+					}
+				);
+			}
+		);
+	});
 	return [dbName, userName];
 };
 
 const drop = (client: any, name: string): Promise<boolean> => {
-	return new Promise(
-		(resolve, reject): void => {
-			client.db(name).dropDatabase(
-				(err: Error, success: boolean): void => {
-					if (err) {
-						reject(err);
-					} else {
-						resolve(success);
-					}
-				}
-			);
-		}
-	);
+	return new Promise((resolve, reject): void => {
+		client.db(name).dropDatabase((err: Error, success: boolean): void => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(success);
+			}
+		});
+	});
 };
 
 export const destroy = async (dbName: string): Promise<void> => {
-	await new Promise(
-		(resolve, reject): void => {
-			MongoClient.connect(
-				'mongodb://localhost:27017',
-				{
-					useNewUrlParser: true
-				},
-				(err, client): any => {
-					if (err) {
-						return reject(err);
-					}
-					client.db(dbName).executeDbAdminCommand(
+	await new Promise((resolve, reject): void => {
+		MongoClient.connect(
+			'mongodb://localhost:27017',
+			{
+				useNewUrlParser: true,
+			},
+			(err, client): any => {
+				if (err) {
+					return reject(err);
+				}
+				client
+					.db(dbName)
+					.executeDbAdminCommand(
 						{listDatabases: 1},
 						(err2: Error, dbNames): any => {
 							if (err2) {
@@ -80,8 +75,7 @@ export const destroy = async (dbName: string): Promise<void> => {
 								.catch(reject);
 						}
 					);
-				}
-			);
-		}
-	);
+			}
+		);
+	});
 };
